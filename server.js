9ff326917cc33dd641c9684628f2e105d8e2fddc
nirmal-k-r <test@test.com>
2026-08-29 12:14:43 +0400

    new

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT ||
 
 
 //start the server
-server.listen(PORT, () => {
+server.listen(PORT,"0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
     console.log(`Site is live at: http://localhost:${PORT}`);
 });
